8ba1b3084568023661f0f2081a8092e9e46639d7
Robin <rtwilborn@gmail.com>
2017-06-01 10:52:49 -0700

    Add Gh and linkedin icons

--- frontend/components/main_page.jsx
+++ frontend/components/main_page.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 const MainPage = () => (
   <div className = "main-page">
-    <div className="splash-navigation">
-      
-    </div>
     <div className="main-content">
-      <h1>Hi, I'm Robin</h1>
-      <h3>I'm a software developer</h3>
+      <h1>HI, I'M ROBIN</h1>
+      <h3>I'M A SOFTWARE DEVELOPER</h3>
+      <div className="splash-icons">
+        <a href="https://www.github.com/wilbooorn"><i className="fa fa-github fa-3x"></i></a>
+        <a href="https://www.linkedin.com/in/rwilborn/"><i className="fa fa-linkedin-square fa-3x"></i></a>
+      </div>
     </div>
-  </div>
+    <div className="splash-navigation">
+      <h3>About</h3>
+      <h3>Portfolio</h3>
+      <h3>Contact</h3>
+      <h3>Resume</h3>
+    </div>
+</div>
 );
 
 export default MainPage;
