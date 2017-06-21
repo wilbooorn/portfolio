66484126ef0c4e6cd235da5038199b7221a07836
Robin <rtwilborn@gmail.com>
2017-06-21 11:13:42 -0700

    Update portfolio page

--- frontend/components/portfolio.jsx
+++ frontend/components/portfolio.jsx
@@ -51,11 +51,39 @@ const Portfolio = () => (
       <h1 className = "portfolio-title">Projects</h1>
       <div className="projects-container">
         <div className="uncappt">
-          <h2>UNCAPP'T</h2>
-          <img src="assets/profile.png" />
+          <div className="uncappt-left">
+            <h2>UNCAPP'T</h2>
+            <img src="assets/profile.png" />
+          </div>
+          <div className="uncappt-right">
+            <p className="uncappt-right-top">UNCAPP'T is a full-stack
+              web application built with a Ruby
+              on Rails back end and a React.js/Redux front end. It is based
+              on the beer rating app <strong>Untappd</strong>. It allows
+              "checkin" new beers when they try them, and see what beers others
+              are drinking around the world.
+            </p>
+            <p className="uncappt-links">
+              You can check out the project live <a href="http://uncappt.com">here</a>
+            or see the code <a href="http://www.github.com/wilbooorn/uncappt">here</a>.
+            </p>
+          </div>
         </div>
 
         <div className="chowtime">
+          <div className="chowtime-left">
+            <p className="chowtime-left-top">Chowtime is a mobile application
+              built with React Native. It is designed to help users decide
+              where to eat by utilizing the Yelp Fusion API to generate a
+              random restaurant based on search criteria. It also utilizes
+              the Google Maps API to provide directions from the user's
+              current geolocation to the restaurant.
+            </p>
+            <p className="chowtime-links">
+              You can check out the demo site <a href="https://wilbooorn.github.io/Chowtime/">here</a>
+            or see the code <a href="http://www.github.com/wilbooorn/chowtime">here</a>.
+            </p>
+          </div>
           <div className="chowtime-right">
             <h2>Chowtime</h2>
             <img src="assets/result.png" />
