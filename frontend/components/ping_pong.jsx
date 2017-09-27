16450ac9a7a7d184460d4c50c0abdbc40c9f6761
Robin <rtwilborn@gmail.com>
2017-09-27 08:09:45 -0700

    Update pong standings

--- frontend/components/ping_pong.jsx
+++ frontend/components/ping_pong.jsx
@@ -22,12 +22,12 @@ class PingPong extends React.Component {
           <div className="standings">
             <div className="robin">
               <h1>Robin</h1>
-              <h2>6</h2>
+              <h2>7</h2>
             </div>
 
             <div className="dustin">
               <h1>Dustin</h1>
-              <h2>4</h2>
+              <h2>5</h2>
             </div>
           </div>
         </div>
